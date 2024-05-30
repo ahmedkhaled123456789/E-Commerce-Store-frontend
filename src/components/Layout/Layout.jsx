f06754d39bfd5ec5685b6ff78950732f51d0c20d
Ahmed Khaled <ahmed1999khaledaboamer1999@gmail.com>
2024-05-30 19:26:51 +0200

    add button scroll

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,6 +3,9 @@ import Routers from '../../Routes/Routers'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Head from '../Header/Head'
+import Louding from '../loading/Louding'
+import Button from '../button/Button'
+import Cuser from '../curser/Curser'
 
 const Layout = () => {
  
@@ -10,6 +13,9 @@ const Layout = () => {
   return (
 
     < > 
+<Louding/>
+<Button/>
+<Cuser/>
     <Head/>
      <Header/>
 
